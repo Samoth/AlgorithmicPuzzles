204ee792e946c2270cdb04795ad01998d9a2d8a4
Samoth <samoth8@gmail.com>
2018-04-25 20:01:03 +0200

    dev: OddOccurrencesInArray task

---
+++ src/pl/florsoft/codility/L02OddOccurrencesInArray.java
@@ -0,0 +1,22 @@
+package pl.florsoft.codility;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Task: https://app.codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/
+ */
+public class L02OddOccurrencesInArray {
+
+    public int solution(int[] A) {
+        Map<Integer, Boolean> oddNumbers = new HashMap<>();
+        for (int i = 0; i < A.length; i++) {
+            if (oddNumbers.get(A[i]) == null) {
+                oddNumbers.put(A[i], true);
+            } else {
+                oddNumbers.remove(A[i]);
+            }
+        }
+        return oddNumbers.keySet().iterator().next(); // return first and only one element
+    }
+}
