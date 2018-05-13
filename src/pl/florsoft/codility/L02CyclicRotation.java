1ca94b2429be71c01807b08a7785ac2d95b84af6
Samoth <samoth8@gmail.com>
2018-05-13 13:06:28 +0200

    dev: Cyclic Rotation task

---
+++ src/pl/florsoft/codility/L02CyclicRotation.java
@@ -0,0 +1,26 @@
+package pl.florsoft.codility;
+
+/**
+ * Task: https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
+ */
+public class L02CyclicRotation {
+
+    public int[] solution(int[] A, int K) {
+        if (A.length == 0 || K % A.length == 0) {
+            return A;
+        }
+        int shiftValue = K % A.length;
+        int[] buffer = new int[shiftValue];
+        for (int i = A.length - 1; i >= 0; i--) {
+            if (i + shiftValue > A.length - 1) {
+                buffer[(i + shiftValue) % A.length] = A[i];
+            } else {
+                A[i + shiftValue] = A[i];
+            }
+        }
+        for (int i = 0; i < buffer.length; i++) {
+            A[i] = buffer[i];
+        }
+        return A;
+    }
+}
