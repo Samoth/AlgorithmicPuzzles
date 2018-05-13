09b8267e33b15d1e9a32602fe5fbf2b9c6c1af7e
Samoth <samoth8@gmail.com>
2018-05-13 20:55:42 +0200

    dev: Count Div task

---
+++ src/pl/florsoft/codility/L05CountDiv.java
@@ -0,0 +1,11 @@
+package pl.florsoft.codility;
+
+public class L05CountDiv {
+
+    public int solution(int A, int B, int K) {
+        int count = A % K == 0 || B % K == 0 ? 1 : 0;
+        double greaterNumber = B < K ? 0 : (double) B / K;
+        double lessNumber = A < K ? 0 : (double) A / K;
+        return count + (int) (greaterNumber - lessNumber);
+    }
+}
