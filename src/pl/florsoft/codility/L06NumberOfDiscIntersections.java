3574631337a9f1e7e4dade37aff3091920701279
Samoth <samoth8@gmail.com>
2018-06-21 19:15:32 +0200

    dev: NumberOfDiscIntersections task - better version

--- src/pl/florsoft/codility/L06NumberOfDiscIntersections.java
+++ src/pl/florsoft/codility/L06NumberOfDiscIntersections.java
@@ -1,8 +1,6 @@
 package pl.florsoft.codility;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -12,8 +10,7 @@ public class L06NumberOfDiscIntersections {
 
     public int solution(int[] A) {
         List<Disc> discsSt = createList(A);
-        List<Disc> discsEn = new ArrayList<>();
-        Collections.copy(discsEn, discsSt);
+        List<Disc> discsEn = new ArrayList<>(discsSt);
         discsSt.sort((o1, o2) -> {
             int result = o1.startPos.compareTo(o2.startPos);
             if (result == 0) {
@@ -35,26 +32,25 @@ public class L06NumberOfDiscIntersections {
         List<Disc> list = new ArrayList<>(A.length);
         for (int i = 0; i < A.length; i++) {
             list.add(new Disc(Math.max(0, i - A[i]),
-                    (int) Math.min((long) A.length, ((long) i + (long) A[i]))));
+                    (int) Math.min((long) A.length - 1, ((long) i + (long) A[i]))));
         }
         return list;
     }
 
-    private static int countElements(List<Disc> discs, List<Disc> descendingDiscs) {
+    private static int countElements(List<Disc> discsSt, List<Disc> discsEn) {
         int count = 0, currentItems = 0, endIdx = 0;
-        for (Disc disc : discs) {
-            currentItems++;
-            while (disc.startPos > descendingDiscs.get(endIdx).endPos) {
-                // TODO check it and fix
+        for (Disc disc : discsSt) {
+            while (disc.startPos > discsEn.get(endIdx).endPos) {
                 endIdx++;
                 currentItems--;
                 count += currentItems;
             }
+            currentItems++;
         }
-//        while (!queue.isEmpty()) {
-//            queue.poll();
-//            count += queue.size();
-//        }
+        while (currentItems != 0) {
+            currentItems--;
+            count += currentItems;
+        }
         return count;
     }
 
@@ -67,4 +63,5 @@ public class L06NumberOfDiscIntersections {
             this.endPos = endPos;
         }
     }
+    
 }
