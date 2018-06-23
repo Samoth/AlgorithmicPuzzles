8714646127cc1426863b54b7d772359e64f1d406
Samoth <samoth8@gmail.com>
2018-06-23 13:22:31 +0200

    dev: L06Distinct task

---
+++ src/pl/florsoft/codility/L06Distinct.java
@@ -0,0 +1,22 @@
+package pl.florsoft.codility;
+
+import java.util.Arrays;
+
+/**
+ * Task: https://app.codility.com/programmers/lessons/6-sorting/distinct/
+ */
+public class L06Distinct {
+
+    public int solution(int[] A) {
+        int distinctVals = 0, prevVal = Integer.MAX_VALUE;
+        Arrays.sort(A);
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] != prevVal) {
+                distinctVals++;
+            }
+            prevVal = A[i];
+        }
+        return distinctVals;
+    }
+
+}
