d3273a292495af580b0c1711cf7d3317a4772636
Samoth <samoth8@gmail.com>
2018-07-02 21:54:16 +0200

    dev: L07Fish task

---
+++ src/pl/florsoft/codility/L07Fish.java
@@ -0,0 +1,29 @@
+package pl.florsoft.codility;
+
+import java.util.Stack;
+
+/**
+ * Task: https://app.codility.com/programmers/lessons/7-stacks_and_queues/fish/
+ */
+public class L07Fish {
+
+    public int solution(int[] A, int[] B) {
+        int fishAlive = A.length;
+        Stack<Integer> fishDownSize = new Stack<>();
+        for (int i = 0; i < B.length; i++) {
+            if (B[i] == 1) {
+                fishDownSize.push(A[i]);
+            } else {
+                while (!fishDownSize.isEmpty() && A[i] > fishDownSize.peek()) {
+                    fishAlive--;
+                    fishDownSize.pop();
+                }
+                if (!fishDownSize.isEmpty() && A[i] < fishDownSize.peek()) {
+                    fishAlive--;
+                }
+            }
+        }
+        return fishAlive;
+    }
+
+}
