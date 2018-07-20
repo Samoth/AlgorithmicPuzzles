776e6f4f49db4a3f88c4436303f0b9579d0ebdf1
Tomasz Florczak <tomasz.florczak@apreel.com>
2018-07-20 15:35:53 +0200

    dev: FindDuplicateInArray - add findDuplicateForSmallArray method

--- src/pl/florsoft/other/findduplicate/FindDuplicateInArray.java
+++ src/pl/florsoft/other/findduplicate/FindDuplicateInArray.java
@@ -21,13 +21,38 @@ public class FindDuplicateInArray {
         return (int) (currentSum - sumWithoutDup);
     }
 
-    public static void main(String[] args) {
-        int[] testArray = new int[]{0, 0};
-        System.out.println("0 ?= " + findOneDuplicate(testArray));
-        int[] testArray2 = new int[]{0, 1, 2, 3, 0};
-        System.out.println("0 ?= " + findOneDuplicate(testArray2));
-        int[] testArray3 = new int[]{5, 3, 2, 5, 1, 4, 0};
-        System.out.println("5 ?= " + findOneDuplicate(testArray3));
+    /**
+     * Many duplicates exists. Only for small array size (less or equals 65)
+     */
+    public static int findDuplicateForSmallArray(int[] A) {
+        if (A.length < 2 || A.length > Long.SIZE + 1) {
+            throw new IllegalArgumentException("Incorrect array");
+        }
+        long numbers = 0;
+        for (int val: A) {
+            long mask = 1L << val;
+            if ((numbers & mask) == 0L) {
+                numbers |= mask;
+            } else {
+                return val;
+            }
+        }
+        return -1;
     }
-    
+
+    public static void main(String[] args) {
+        int[] testArray = new int[]{1, 1};
+        System.out.println("1 ?= " + findDuplicateForSmallArray(testArray));
+        int[] testArray2 = new int[]{0, 1, 2, 3, 0};
+        System.out.println("0 ?= " + findDuplicateForSmallArray(testArray2));
+        int[] testArray3 = new int[]{5, 3, 2, 4, 1, 4, 0};
+        System.out.println("4 ?= " + findDuplicateForSmallArray(testArray3));
+        int[] testArray4 = new int[65];
+        for (int i = 0; i < testArray4.length - 1; i++) {
+            testArray4[i] = i;
+        }
+        testArray4[64] = 56;
+        System.out.println("56 ?= " + findDuplicateForSmallArray(testArray4));
+    }
+
 }
