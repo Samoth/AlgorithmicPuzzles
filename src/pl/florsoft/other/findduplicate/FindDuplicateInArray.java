c1c819c9e90fc06b134e8e5e0d6f7241eb70775c
Samoth <samoth8@gmail.com>
2018-08-03 08:22:34 +0200

    dev: FindDuplicateInArray: divide and conquer solution

--- src/pl/florsoft/other/findduplicate/FindDuplicateInArray.java
+++ src/pl/florsoft/other/findduplicate/FindDuplicateInArray.java
@@ -2,10 +2,13 @@ package pl.florsoft.other.findduplicate;
 
 /**
  * Task: given A array consisting N+1 integers within the range [0..N-1]. So there is at least one duplicate.
- * Find duplicate number.
+ * Find duplicate number, using O(1) memory.
  */
 public class FindDuplicateInArray {
 
+    /**
+     * Find a cycle in a graph.
+     */
     public static int findDuplicate(int[] A) {
         int startElem = A[A.length - 1];
         if (A[startElem] == startElem) {
@@ -29,6 +32,38 @@ public class FindDuplicateInArray {
     }
 
     /**
+     * Find duplicate elements. Time complexity: O(n*logn).
+     */
+    public static int findDuplicateDivideAndConquer(int[] A) {
+        if (A.length < 2) {
+            throw new IllegalArgumentException("Incorrect array");
+        }
+        return findDuplicateLogN(A, 0, A.length - 2);
+    }
+
+    private static int findDuplicateLogN(int[] A, int min, int max) {
+        if (min == max) {
+            return min;
+        } else if (min > max) {
+            throw new IllegalArgumentException("Incorrect array. No duplicates.");
+        }
+        int sum = 0, elems = 0;
+        for (int val : A) {
+            if (val >= min && val <= max) {
+                sum += val;
+                elems++;
+            }
+        }
+        double avg = (double) sum / elems;
+        double middleElem = (double) (min + max) / 2;
+        if (avg <= middleElem) {
+            return findDuplicateLogN(A, min, (int) middleElem);
+        } else {
+            return findDuplicateLogN(A, (int) middleElem + 1, max);
+        }
+    }
+
+    /**
      * Only one duplicate exists.
      */
     public static int findOneDuplicate(int[] A) {
@@ -37,7 +72,7 @@ public class FindDuplicateInArray {
         }
         long sumWithoutDup = (long) (A.length - 2) * (A.length - 1) / 2;
         long currentSum = 0;
-        for (int val: A) {
+        for (int val : A) {
             currentSum += val;
         }
         return (int) (currentSum - sumWithoutDup);
@@ -51,7 +86,7 @@ public class FindDuplicateInArray {
             throw new IllegalArgumentException("Incorrect array");
         }
         long numbers = 0;
-        for (int val: A) {
+        for (int val : A) {
             long mask = 1L << val;
             if ((numbers & mask) == 0L) {
                 numbers |= mask;
@@ -63,16 +98,16 @@ public class FindDuplicateInArray {
     }
 
     public static void main(String[] args) {
-        int[] testArray = new int[]{1, 1};
-        System.out.println("1 ?= " + findDuplicate(testArray));
+        int[] testArray = new int[]{1, 1, 0};
+        System.out.println("1 ?= " + findDuplicateDivideAndConquer(testArray));
         int[] testArray2 = new int[]{0, 1, 2, 3, 0};
-        System.out.println("0 ?= " + findDuplicate(testArray2));
+        System.out.println("0 ?= " + findDuplicateDivideAndConquer(testArray2));
         int[] testArray3 = new int[]{5, 3, 2, 5, 1, 4, 0};
-        System.out.println("5 ?= " + findDuplicate(testArray3));
+        System.out.println("5 ?= " + findDuplicateDivideAndConquer(testArray3));
         int[] testArray4 = new int[]{6, 1, 4, 2, 2, 3, 0, 5};
-        System.out.println("2 ?= " + findDuplicate(testArray4));
+        System.out.println("2 ?= " + findDuplicateDivideAndConquer(testArray4));
         int[] testArray5 = new int[]{2, 1, 3, 0, 0, 0, 2};
-        System.out.println("2 ?= " + findDuplicate(testArray5));
+        System.out.println("0 ?= " + findDuplicateDivideAndConquer(testArray5));
     }
 
 }
