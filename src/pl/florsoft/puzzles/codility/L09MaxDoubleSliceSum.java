e3f50cc24eba3c9b4aed831466bd9aa01fba2ee8
Tomasz Florczak <tomasz.florczak@apreel.com>
2018-08-07 17:43:21 +0200

    dev: task L09MaxDoubleSliceSum - tmp version

---
+++ src/pl/florsoft/puzzles/codility/L09MaxDoubleSliceSum.java
@@ -0,0 +1,39 @@
+package pl.florsoft.puzzles.codility;
+
+/**
+ * Task: https://app.codility.com/programmers/lessons/9-maximum_slice_problem/max_double_slice_sum/
+ */
+public class L09MaxDoubleSliceSum {
+
+    public int solution(int[] A) {
+        int currentSum = 0, maxDoubleSlice = 0, minVal = A[1], prevSum = 0;
+        for (int i = 1; i < A.length - 1; i++) {
+            int val = A[i];
+            if (currentSum + val - minVal > 0) {
+                currentSum += val;
+                maxDoubleSlice = Math.max(maxDoubleSlice, currentSum - minVal);
+            } else {
+                currentSum = currentSum + val - prevSum;
+                prevSum = currentSum;
+                //currentSum = 0;
+                minVal = val;
+            }
+            if (val < minVal) {
+                minVal = val;
+                prevSum = currentSum;
+            }
+            // currentSum = Math.max(0, currentSum + val);
+
+            // System.out.println("val = " + val + ", minVal = " + minVal + ", currentSum = " + currentSum + ", max = " + maxDoubleSlice);
+
+        }
+        return maxDoubleSlice;
+    }
+
+    public static void main(String[] args) {
+        int[] test1 = {2, 3, -1231, 1, 3, -10000, 3, 1, 2, -111, 4, 3};
+        int[] test2 = {-2, -3, -4, 1, -5, -6, -7};
+        int[] test3 = {5, 17, 0, 3};
+    }
+
+}
