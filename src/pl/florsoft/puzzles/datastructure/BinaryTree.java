471d98236ba58d32659b1e2d490ecad1c129b9d2
Samoth <samoth8@gmail.com>
2018-09-27 21:45:40 +0200

    dev: BinaryTree structure

---
+++ src/pl/florsoft/puzzles/datastructure/BinaryTree.java
@@ -0,0 +1,50 @@
+package pl.florsoft.puzzles.datastructure;
+
+public class BinaryTree<E> {
+
+    public BinaryTreeNode root;
+
+    public BinaryTree() {
+    }
+
+    public BinaryTree(BinaryTreeNode root) {
+        this.root = root;
+    }
+
+    public void inOrderTraversal(BinaryTreeNode<E> treeNode) {
+        if (treeNode != null) {
+            inOrderTraversal(treeNode.left);
+            System.out.println(treeNode.value);
+            inOrderTraversal(treeNode.right);
+        }
+    }
+
+    public void preOrderTraversal(BinaryTreeNode<E> treeNode) {
+        if (treeNode != null) {
+            System.out.println(treeNode.value);
+            preOrderTraversal(treeNode.left);
+            preOrderTraversal(treeNode.right);
+        }
+    }
+
+    public void postOrderTraversal(BinaryTreeNode<E> treeNode) {
+        if (treeNode != null) {
+            postOrderTraversal(treeNode.left);
+            postOrderTraversal(treeNode.right);
+            System.out.println(treeNode.value);
+        }
+    }
+
+    class BinaryTreeNode<E> {
+
+        public E value;
+        public BinaryTreeNode<E> left;
+        public BinaryTreeNode<E> right;
+
+        public BinaryTreeNode(E value) {
+            this.value = value;
+        }
+
+    }
+
+}
