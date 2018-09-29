3395a9ae78d259628e9aae79e8ab92ac4a5121ed
Samoth <samoth8@gmail.com>
2018-09-29 10:30:10 +0200

    dev: Graph structure

--- src/pl/florsoft/puzzles/datastructure/Graph.java
+++ src/pl/florsoft/puzzles/datastructure/Graph.java
@@ -1,6 +1,20 @@
 package pl.florsoft.puzzles.datastructure;
 
-public class Graph {
+import java.util.List;
 
+public class Graph<E> {
+
+    public List<GraphNode> nodes;
+
+    static class GraphNode<E> {
+
+        public E value;
+        public List<GraphNode> children;
+
+        public GraphNode(E value) {
+            this.value = value;
+        }
+
+    }
 
 }
