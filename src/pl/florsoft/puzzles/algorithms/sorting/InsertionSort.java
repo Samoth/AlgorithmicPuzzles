cf0cda8baeaf5ec8c1169eb397f2c6f836223e17
Samoth <samoth8@gmail.com>
2018-09-30 10:02:25 +0200

    dev: InsertionSort algorithm

--- src/pl/florsoft/puzzles/algorithms/sorting/InsertionSort.java
+++ src/pl/florsoft/puzzles/algorithms/sorting/InsertionSort.java
@@ -1,4 +1,20 @@
 package pl.florsoft.puzzles.algorithms.sorting;
 
 public class InsertionSort {
+
+    public static void sort(int[] array) {
+        if (array == null || array.length <= 1) {
+            return;
+        }
+        for (int i = 1; i < array.length; i++) {
+            int currentElem = array[i];
+            int j = i - 1;
+            while (j >= 0 && currentElem < array[j]) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = currentElem;
+        }
+    }
+
 }
