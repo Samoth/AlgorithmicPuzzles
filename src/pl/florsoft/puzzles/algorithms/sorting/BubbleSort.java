4b7f1ea3d0eb6471723b5ea7644756e5e93c869c
Samoth <samoth8@gmail.com>
2018-09-30 09:36:14 +0200

    dev: BubbleSort algorithm

--- src/pl/florsoft/puzzles/algorithms/sorting/BubbleSort.java
+++ src/pl/florsoft/puzzles/algorithms/sorting/BubbleSort.java
@@ -8,6 +8,9 @@ package pl.florsoft.puzzles.algorithms.sorting;
 public class BubbleSort {
 
     public static void sort(int[] array) {
+        if (array == null || array.length <= 1) {
+            return;
+        }
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length - 1 - i; j++) {
                 if (array[j] > array[j + 1]) {
