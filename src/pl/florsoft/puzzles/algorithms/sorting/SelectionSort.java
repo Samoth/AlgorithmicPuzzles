6e6a2bd57b612cc5cdd8b6715b4d24e08307f12d
Samoth <samoth8@gmail.com>
2018-09-30 10:17:35 +0200

    dev: SelectionSort algorithm

--- src/pl/florsoft/puzzles/algorithms/sorting/SelectionSort.java
+++ src/pl/florsoft/puzzles/algorithms/sorting/SelectionSort.java
@@ -1,4 +1,29 @@
 package pl.florsoft.puzzles.algorithms.sorting;
 
+/**
+ * Selection sort algorithm.
+ * Time complexity: O(n*n).
+ * Memory complexity: O(1).
+ */
 public class SelectionSort {
+
+    public static void sort(int[] array) {
+        if (array == null || array.length <= 1) {
+            return;
+        }
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            if (minIdx != i) {
+                int tmp = array[i];
+                array[i] = array[minIdx];
+                array[minIdx] = tmp;
+            }
+        }
+    }
+
 }
